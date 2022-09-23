c2c7667d11637b904f72d80f0b19f806a1da1641
aubry prieur <aubry.prieur@gmail.com>
2022-09-23 11:15:23 +0200

    added activité qpv - tx emploi and tx precaire

--- pages/activite.py
+++ pages/activite.py
@@ -43,8 +43,8 @@ def app():
   #Année
   select_annee = st.sidebar.select_slider(
        "Sélection de l'année",
-       options=['2014', '2015', '2016', '2017', '2018'],
-       value=('2018'))
+       options=['2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022'],
+       value=('2022'))
   st.sidebar.write('Mon année :', select_annee)
 
   #############################################################################
@@ -52,6 +52,53 @@ def app():
   st.title('ACTIVITÉ ET EMPLOI')
 
   st.header("Taux d'emploi des 15-64 ans")
+  st.subheader("Zoom QPV")
+
+  def tx_emploi_qpv(fichier, nom_ville, annee) :
+    fp_qpv = "./qpv.geojson"
+    map_qpv_df = gpd.read_file(fp_qpv)
+    year = annee[-2:]
+    df = pd.read_csv(fichier, dtype={"CODGEO": str}, sep=";")
+    map_qpv_df_code_insee = map_qpv_df.merge(df, left_on='code_qp', right_on='CODGEO')
+    map_qpv_df_code_insee_extract = map_qpv_df_code_insee[['nom_qp', 'code_qp', 'commune_qp','code_insee', 'TX_TOT_EMPL' ]]
+    map_qpv_df_code_insee_extract
+    df_qpv = map_qpv_df_code_insee_extract.loc[map_qpv_df_code_insee_extract["commune_qp"].str.contains(nom_ville + "(,|$)")]
+    df_qpv = df_qpv.reset_index(drop=True)
+    df_qpv = df_qpv[['code_qp', 'nom_qp','commune_qp', 'TX_TOT_EMPL']]
+    df_qpv = df_qpv.rename(columns={'nom_qp': "Nom du quartier",'code_qp' : "Code du quartier", "commune_qp" : "Communes concernées", 'TX_TOT_EMPL' : "Taux d'emploi des 15/65 ans " + select_annee})
+    return df_qpv
+
+  tx_emploi_qpv = tx_emploi_qpv('./activite/insertion-pro-qpv/IPRO_' + select_annee + '.csv', nom_commune, select_annee)
+  st.table(tx_emploi_qpv)
+
+  st.header("Part des emplois à durée limitée parmi les emplois")
+  st.subheader("Zoom QPV")
+
+  def tx_emploi_limit_qpv(fichier, nom_ville, annee) :
+    fp_qpv = "./qpv.geojson"
+    map_qpv_df = gpd.read_file(fp_qpv)
+    year = annee[-2:]
+    df = pd.read_csv(fichier, dtype={"CODGEO": str}, sep=";")
+    map_qpv_df_code_insee = map_qpv_df.merge(df, left_on='code_qp', right_on='CODGEO')
+    if int(annee) >= 2021:
+      map_qpv_df_code_insee_extract = map_qpv_df_code_insee[['nom_qp', 'code_qp', 'commune_qp','code_insee', 'TX_TOT_EDLIM' ]]
+      map_qpv_df_code_insee_extract
+      df_qpv = map_qpv_df_code_insee_extract.loc[map_qpv_df_code_insee_extract["commune_qp"].str.contains(nom_ville + "(,|$)")]
+      df_qpv = df_qpv.reset_index(drop=True)
+      df_qpv = df_qpv[['code_qp', 'nom_qp','commune_qp', 'TX_TOT_EDLIM']]
+      df_qpv = df_qpv.rename(columns={'nom_qp': "Nom du quartier",'code_qp' : "Code du quartier", "commune_qp" : "Communes concernées", 'TX_TOT_EDLIM' : "Part des emplois à durée limitée " + select_annee})
+    else:
+      map_qpv_df_code_insee_extract = map_qpv_df_code_insee[['nom_qp', 'code_qp', 'commune_qp','code_insee', 'TX_TOT_EPREC' ]]
+      map_qpv_df_code_insee_extract
+      df_qpv = map_qpv_df_code_insee_extract.loc[map_qpv_df_code_insee_extract["commune_qp"].str.contains(nom_ville + "(,|$)")]
+      df_qpv = df_qpv.reset_index(drop=True)
+      df_qpv = df_qpv[['code_qp', 'nom_qp','commune_qp', 'TX_TOT_EPREC']]
+      df_qpv = df_qpv.rename(columns={'nom_qp': "Nom du quartier",'code_qp' : "Code du quartier", "commune_qp" : "Communes concernées", 'TX_TOT_EPREC' : "Part des emplois à durée limitée " + select_annee})
+    return df_qpv
+
+  tx_emploi_limit_qpv = tx_emploi_limit_qpv('./activite/insertion-pro-qpv/IPRO_' + select_annee + '.csv', nom_commune, select_annee)
+  st.table(tx_emploi_limit_qpv)
+
   st.header("Part des salariés en emploi précaires")
   st.header("Demandeurs de longue durée (2 ans ou plus)")
   st.header("Evolution du nombre de DEFM de catégorie ABC [base 100 à ...]")
