3e08218e3fd9ed68f48d777a90a682b6a73bb41b
aubry prieur <aubry.prieur@gmail.com>
2024-05-28 16:21:31 +0200

    Updated taux mortalité

--- pages/sante.py
+++ pages/sante.py
@@ -28,44 +28,45 @@ def app(code_commune, nom_commune, code_epci, nom_epci, code_departement, nom_de
               à structure d'âge équivalente ou avec le taux de mortalité prématuré qui ne s'intéresse qu'aux décès intervenus avant 65 ans.")
 
   #Commune
-  def tx_mortalite_commune(fichier, nom_ville) :
+  period = "2014-2020"
+  last_year_mortal = "2020"
+  def tx_mortalite_commune(fichier, nom_ville, period) :
     df = pd.read_csv(fichier, dtype={"codgeo": str, "an": str},sep=";")
     df_ville = df.loc[df["libgeo"] == nom_ville]
-    df_ville = df_ville.loc[df_ville["an"] == "2013-2018"]
+    df_ville = df_ville.loc[df_ville["an"] == period ]
     return df_ville
-  tx_mortalite_ville = tx_mortalite_commune("./sante/taux_de_mortalite/insee_rp_evol_1968_communes_2018.csv",nom_commune)
+  tx_mortalite_ville = tx_mortalite_commune("./sante/taux_de_mortalite/insee_rp_evol_1968_communes_" + last_year_mortal + ".csv",nom_commune, period)
 
   #EPCI
-  def tx_mortalite_epci(fichier, epci) :
+  def tx_mortalite_epci(fichier, epci, period) :
     df = pd.read_csv(fichier, dtype={"codgeo": str, "an": str},sep=";")
     df_epci = df.loc[df["codgeo"] == epci]
-    df_epci = df_epci.loc[df_epci["an"] == "2013-2018"]
+    df_epci = df_epci.loc[df_epci["an"] == period]
     return df_epci
-  tx_mortalite_epci = tx_mortalite_epci("./sante/taux_de_mortalite/insee_rp_evol_1968_epci_2018.csv",code_epci)
+  tx_mortalite_epci = tx_mortalite_epci("./sante/taux_de_mortalite/insee_rp_evol_1968_epci_" + last_year_mortal + ".csv",code_epci, period)
 
   #Département
-  def tx_mortalite_departement(fichier, departement) :
+  def tx_mortalite_departement(fichier, departement, period) :
     df = pd.read_csv(fichier, dtype={"codgeo": str, "an": str},sep=";")
     df_departement = df.loc[df["codgeo"] == departement]
-    df_departement = df_departement.loc[df_departement["an"] == "2013-2018"]
+    df_departement = df_departement.loc[df_departement["an"] == period]
     return df_departement
-  tx_mortalite_dpt = tx_mortalite_departement("./sante/taux_de_mortalite/insee_rp_evol_1968_departement_2018.csv",code_departement)
+  tx_mortalite_dpt = tx_mortalite_departement("./sante/taux_de_mortalite/insee_rp_evol_1968_departement_" + last_year_mortal + ".csv",code_departement, period)
 
   #Région
-  def tx_mortalite_region(fichier, region) :
+  def tx_mortalite_region(fichier, region, period) :
     df = pd.read_csv(fichier, dtype={"codgeo": str, "an": str},sep=";")
     df_region = df.loc[df["codgeo"] == region]
-    df_region = df_region.loc[df_region["an"] == "2013-2018"]
+    df_region = df_region.loc[df_region["an"] == period]
     return df_region
-  tx_mortalite_reg = tx_mortalite_region("./sante/taux_de_mortalite/insee_rp_evol_1968_region_2018.csv",code_region)
+  tx_mortalite_reg = tx_mortalite_region("./sante/taux_de_mortalite/insee_rp_evol_1968_region_" + last_year_mortal + ".csv",code_region, period)
 
   #France
-  data = {'codgeo':['1'],
-          'libgeo':['France'],
-          'an':['2013-2018'],
-          'tx_morta':['8,8']
-          }
-  tx_mortalite_france = pd.DataFrame(data)
+  def tx_mortalite_france(fichier, period) :
+    df = pd.read_csv(fichier, dtype={"codgeo": str, "an": str},sep=";")
+    df_france = df.loc[df["an"] == period]
+    return df_france
+  tx_mortalite_france = tx_mortalite_france("./sante/taux_de_mortalite/insee_rp_evol_1968_france_" + last_year_mortal + ".csv", period)
 
   #Global
   result = pd.concat([tx_mortalite_ville,tx_mortalite_epci, tx_mortalite_dpt, tx_mortalite_reg, tx_mortalite_france])
