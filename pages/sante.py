4304ea8c7725f1a6bb0a71524ab83212892d12ea
aubry prieur <aubry.prieur@gmail.com>
2024-06-03 16:45:59 +0200

    Added a graph and tab system to the mortality rate

--- pages/sante.py
+++ pages/sante.py
@@ -36,47 +36,57 @@ def app(code_commune, nom_commune, code_epci, nom_epci, code_departement, nom_de
   #Commune
   period = "2014-2020"
   last_year_mortal = "2020"
-  def tx_mortalite_commune(fichier, code_ville, period) :
-    df = pd.read_csv(fichier, dtype={"codgeo": str, "an": str},sep=";")
-    df_ville = df.loc[df["codgeo"] == code_ville]
-    df_ville = df_ville.loc[df_ville["an"] == period ]
-    return df_ville
-  tx_mortalite_ville = tx_mortalite_commune("./sante/taux_de_mortalite/insee_rp_evol_1968_communes_" + last_year_mortal + ".csv",code_commune, period)
 
-  #EPCI
-  def tx_mortalite_epci(fichier, epci, period) :
-    df = pd.read_csv(fichier, dtype={"codgeo": str, "an": str},sep=";")
-    df_epci = df.loc[df["codgeo"] == epci]
-    df_epci = df_epci.loc[df_epci["an"] == period]
-    return df_epci
-  tx_mortalite_epci = tx_mortalite_epci("./sante/taux_de_mortalite/insee_rp_evol_1968_epci_" + last_year_mortal + ".csv",code_epci, period)
+  # Charger et filtrer les données pour chaque niveau géographique
+  def load_data(fichier, code, period):
+      df = pd.read_csv(fichier, dtype={"codgeo": str}, sep=';')
+      df = df.loc[df["codgeo"] == code]
+      df = df.loc[df["an"] == period]
+      return df
 
-  #Département
-  def tx_mortalite_departement(fichier, departement, period) :
-    df = pd.read_csv(fichier, dtype={"codgeo": str, "an": str},sep=";")
-    df_departement = df.loc[df["codgeo"] == departement]
-    df_departement = df_departement.loc[df_departement["an"] == period]
-    return df_departement
-  tx_mortalite_dpt = tx_mortalite_departement("./sante/taux_de_mortalite/insee_rp_evol_1968_departement_" + last_year_mortal + ".csv",code_departement, period)
+  tx_mortalite_ville = load_data("./sante/taux_de_mortalite/insee_rp_evol_1968_communes_" + last_year_mortal + ".csv", code_commune, period)
+  tx_mortalite_epci = load_data("./sante/taux_de_mortalite/insee_rp_evol_1968_epci_" + last_year_mortal + ".csv", code_epci, period)
+  tx_mortalite_dpt = load_data("./sante/taux_de_mortalite/insee_rp_evol_1968_departement_" + last_year_mortal + ".csv", code_departement, period)
+  tx_mortalite_reg = load_data("./sante/taux_de_mortalite/insee_rp_evol_1968_region_" + last_year_mortal + ".csv", code_region, period)
 
-  #Région
-  def tx_mortalite_region(fichier, region, period) :
-    df = pd.read_csv(fichier, dtype={"codgeo": str, "an": str},sep=";")
-    df_region = df.loc[df["codgeo"] == region]
-    df_region = df_region.loc[df_region["an"] == period]
-    return df_region
-  tx_mortalite_reg = tx_mortalite_region("./sante/taux_de_mortalite/insee_rp_evol_1968_region_" + last_year_mortal + ".csv",code_region, period)
+  # France
+  def load_data_france(fichier, period):
+      df = pd.read_csv(fichier, dtype={"codgeo": str}, sep=';')
+      df = df.loc[df["an"] == period]
+      return df
 
-  #France
-  def tx_mortalite_france(fichier, period) :
-    df = pd.read_csv(fichier, dtype={"codgeo": str, "an": str},sep=";")
-    df_france = df.loc[df["an"] == period]
-    return df_france
-  tx_mortalite_france = tx_mortalite_france("./sante/taux_de_mortalite/insee_rp_evol_1968_france_" + last_year_mortal + ".csv", period)
+  tx_mortalite_france = load_data_france("./sante/taux_de_mortalite/insee_rp_evol_1968_france_" + last_year_mortal + ".csv", period)
 
-  #Global
-  result = pd.concat([tx_mortalite_ville,tx_mortalite_epci, tx_mortalite_dpt, tx_mortalite_reg, tx_mortalite_france])
-  st.write(result)
+  # Fusionner les données
+  result = pd.concat([tx_mortalite_ville, tx_mortalite_epci, tx_mortalite_dpt, tx_mortalite_reg, tx_mortalite_france])
+
+  # Épurer le tableau
+  result = result[['libgeo', 'tx_morta']]
+  result.columns = ['Territoires', 'Taux de mortalité']
+  result['Taux de mortalité'] = result['Taux de mortalité'].str.replace(',', '.').astype(float)
+  result.reset_index(drop=True, inplace=True)
+
+  # Créer le graphique interactif en barres horizontales
+  fig = px.bar(result, x='Taux de mortalité', y='Territoires', orientation='h',
+               title='Comparaison du taux de mortalité')
+
+  # Créer le menu d'options pour les onglets
+  selected = option_menu(
+      menu_title=None,  # required
+      options=["Tableau", "Graphique"],  # required
+      icons=["table", "bar-chart"],  # optional
+      menu_icon="cast",  # optional
+      default_index=0,  # optional
+      orientation="horizontal",
+      key="key10"
+  )
+
+  # Afficher le contenu basé sur l'onglet sélectionné
+  if selected == "Tableau":
+      st.write("Données des taux de mortalité")
+      st.dataframe(result)
+  elif selected == "Graphique":
+      st.plotly_chart(fig)
   ############################################################################
   st.header("2.L'accessibilité aux soins")
   st.subheader("Accessibilité potentielle localisée (APL) à la médecine libérale")
